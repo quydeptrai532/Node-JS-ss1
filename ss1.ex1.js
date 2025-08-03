0fdf50c8d2cc2c6517efe9ecde96e22319050ba0
quydeptrai532 <viquy16@gmail.com>
2025-08-03 17:36:31 +0700

    Xóa tất cả file khỏi Git nhưng giữ file trên máy

--- ss1.ex1.js
+++
@@ -1,5 +0,0 @@
-for (let i=0;i<5;i++){
-    console.log(i);
-}
-console.log(i);
-//let variable gia tri chi dung duoc trong vong lap
--- ss1.ex6.js
+++
@@ -1,5 +0,0 @@
-function createHuman(name,age=18,role=`user`){
-     console.log(`${name} ${age} ${role}`);
-}
-
-createHuman(`Quydz`)
